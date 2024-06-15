78e94b54e77163d6e211085b1e21a8cf68ae851e
elotwo <obiagwu94@gmail.com>
2024-06-15 21:47:07 +0100

    me

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -25,8 +25,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	parent->left = new_node;
-	new_node->parent = parent;
+	parent->left = new_node;                                   new_node->parent = parent;
 	return new_node;
 }
 
---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * 
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return(0);
+	}
+	size_t count_left = binary_tree_depth(tree->left);
+	size_t count_right = binary_tree_depth(tree->right);
+	if (count_left > count_right)
+	{
+		return (count_left + 1);
+	}
+	else
+	{
+		return(count_right + 1);
+	}
+}
