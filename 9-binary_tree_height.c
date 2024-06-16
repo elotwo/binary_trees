55969d59cb83766b81a3297601c568587ffe47e0
elotwo <obiagwu94@gmail.com>
2024-06-16 16:56:51 +0100

    me

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,10 @@
 #include <math.h>
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_height - this function is about measure the
+ * height of a binary tree by comparing the left and right nodes
+ * @tree: is the target node
+ * Return: (0)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -11,13 +14,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
-	
+
 	if (left_height > right_height)
 	{
-		return(left_height + 1);
+		return (left_height + 1);
 	}
 	else
 	{
-		return(right_height + 1);
+		return (right_height + 1);
 	}
 }
