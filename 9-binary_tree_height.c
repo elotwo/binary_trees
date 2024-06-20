25a1a51ded8525e117ba2453fff45b8189f76eb1
elotwo <obiagwu94@gmail.com>
2024-06-20 13:37:56 +0100

    me

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,16 +11,17 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
 	{
-		return (left_height + 1);
+		return (0);
 	}
 	else
 	{
-		return (right_height + 1);
+		size_t left_height = binary_tree_height(tree->left);
+		size_t right_height = binary_tree_height(tree->right);
+		
+		if (left_height > right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
 	}
 }
